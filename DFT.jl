4feea3fafd1b9aab4cf3932f21efa46bd4f6b0ff
syy <syyang@bu.edu>
2020-12-28 19:30:31 -0500

    Need to figure out discrete inverse fourier tranform

--- DFT.jl
+++ DFT.jl
@@ -1,31 +1,34 @@
 using Plots
 
 a = [1 -1; 0 2; -1 -1]
-nP = 3
+a = [ 2 0;  2  1;  2  2;  2  3;  1  3;  1  2;  1  1; 
+       0 1; -1  1; -1  2; -1  3; -2  3; -2  2; -2  1;
+      -2 0; -2 -1; -2 -2; -2 -3; -1 -3; -1 -2; -1 -1;
+      0 -1;  1 -1;  1 -2;  1 -3;  2 -1;  2 -2;  2 -3]
+
+nP = size(a)[1]
 
 # T sequence
-N = 100 
-t_seq = [(i-1) * 2 * pi / N for i in 1:100]
+N = nP 
+t_seq = [(i-1) * 2 * pi / N for i in 1:N]
 
 plot(a[:, 1], a[:, 2], seriestype = :scatter)  
-# The 3 points connected path is used to calculate DFT and draw path using epicycloid.
-# 3 points are too few.. but whatever... 
 
-K = 3
+K = nP
 # ^ highest frequency
 
 x_k = []
 
 for k in 1:K
 	res = 0
-	for i in 1:3
+	for i in 1:nP
 		res += (a[i, 1] + a[i, 2] *im) * exp( - k * 2*pi / nP * (i-1) *im )
 	end
 	Base.push!(x_k, res)
 end
 
 # Compute end points of epicycloid 
-ec = [0 for i in 1:100] 
+ec = [0 for i in 1:N] 
 for k in 1:K
 	ec += x_k[k] * exp.( t_seq * im * k )
 end
@@ -35,6 +38,9 @@ ec /= nP
 plot!( real.(ec), imag.(ec) , seriestype = :scatter)
 
 
-# When highest frequency is set to be 3 (same as #points), the points are on the path
-# But when highest frequency is > 3 (i.g. as 5), I get weird shape.. kind of make sense
+# Why number of points have to be the same as the number of frequencies??
+## When I set N > nP
+## I get weird shapes
+## But when I set N = nP
+## I can get all the exact points from the original input 
 
