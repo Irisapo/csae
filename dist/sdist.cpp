3c5c35eb252dcb979f49424f464e4c8120d87fc8
syy <syyang@bu.edu>
2021-05-08 19:27:37 -0400

    Delete garbage

--- dist/sdist.cpp
+++ dist/sdist.cpp
@@ -1,15 +1,16 @@
 #include <cmath>
 #include <vector>
 #include <random> // get  uniform rv 
+#include "boost/math/special_functions/bessel.hpp"  // use cyl_bessel_i
 
 std::vector<float> rv_exp( float lambda,  int n, unsigned seed = 1111)
 {
     std::vector<float> rv(n);
 
-    std::mt19937 generator (seed);
+    std::mt19937 gt (seed);
     std::uniform_real_distribution<float> RV_u1(0., 1.);
     for (int i=0; i<n; i++) {
-        rv[i] = -1. / lambda * std::log( RV_u1(generator) );
+        rv[i] = -1. / lambda * std::log( RV_u1(gt) );
     }
 
     return rv;
@@ -20,88 +21,16 @@ std::vector<float> rv_b1 ( float shape,  int n, unsigned seed=1111)
 {
     std::vector<float> rv(n);
 
-    std::mt19937 generator (seed);
+    std::mt19937 gt (seed);
     std::uniform_real_distribution<float> RV_u1(0., 1.), RV_u2(0., 1.);
     float u1, u2;
     for (int i=0; i<n; i++) {
         do{
-            u1 = RV_u1(generator); 
-            u2 = RV_u2(generator);
+            u1 = RV_u1(gt); 
+            u2 = RV_u2(gt);
         } while(u2 > std::pow(4. * u1 * (1.-u1),  shape)); 
         rv[i] = u1;
     }
     return rv;
 }
-
-
-float log_pm_unnorm (float a, float nu, float m)
-{
-    float logpm_unnorm;
-
-    logpm_unnorm = (2*m+nu) * std::log(a/2.) - std::lgamma(m+1) - std::lgamma(m+nu+1);
-
-    return logpm_unnorm;
-}
-
-// prob of bes(nu,a, x=m)
-float p_m(float a, float nu, float m)
-{
-    float logpm_unnorm, pm;
-    float norm_bes;
-
-    norm_bes = std::cyl_bessel_if(nu, a);
-
-    logpm_unnorm = log_pm_unnorm(a, nu, m);
-    pm = std::exp(logpm_unnorm) / norm_bes;
-
-    return pm;
-}
-
-std::vector<int> rv1_bes ( float a,  float nu,  int n, unsigned seed=1111)
-{
-    // nu > -1, a > 0 
-    std::vector<int> rv(n);
-
-    float m;
-    m = std::round( (std::sqrt(a*a+nu*nu) - nu)/2. );
-    
-    float pm;
-    pm = p_m(a, nu, m); 
-
-    float w = 1. + pm/2.;
-
-    std::uniform_real_distribution<float> RV_u(0., 1.);
-    std::exponential_distribution<float> RV_e(1.);
-    std::bernoulli_distribution RV_b(0.5);
-    std::mt19937 generator (seed);
-    float u1, u2;
-    float x, y;
-    float pmx, j;
-    bool s;
-
-
-    for(int i=0; i<n; i++) {
-        do{
-            u1 = RV_u(generator);
-            u2 = RV_u(generator);
-            s = RV_b(generator);
-            y = (u1 < w/(1+w)) ? w*RV_u(generator)/pm : (w+RV_e(generator))/pm ;
-            x = std::round(y);
-            if (s == false) {
-                x *= -1; 
-            }
-            pmx = p_m(a, nu, m+x);
-            j = std::log(w);
-            if ( (u2-pm*y)<0 ) {
-                j += u2-pm*y;
-            }
-        } while( j > std::log(pmx)-std::log(pm) );
-        rv[i] = m+x;
-    }
-
-    return rv;
-    
-}
-
-
 
