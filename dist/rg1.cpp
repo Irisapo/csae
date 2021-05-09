5a453aa2fe0e292e6ea146f31b4847a5939389d1
syy <syyang@bu.edu>
2021-05-09 11:12:52 -0400

    Generate rv from RG1

--- dist/rg1.cpp
+++ dist/rg1.cpp
@@ -1,6 +1,6 @@
 #include <cmath>
 #include <vector>
-#include <random> // get  uniform rv
+#include <random> 
 #include "boost/math/special_functions/bessel.hpp"  // use cyl_bessel_i
 
 float logp_rg1 (float lambda, float a, float b, float x)
@@ -20,3 +20,20 @@ float logp_rg1 (float lambda, float a, float b, float x)
 }
 
 
+std::vector<float> rv_rg1(float lambda, float a, float b, int n, unsigned seed=1111)
+{
+    std::vector<float> rv(n);
+    std::mt19937 gen(seed); 
+    
+    std::poisson_distribution<int> RV_poi(lambda);
+
+    int sp;
+    for (int i=0; i<n; i++) {
+        sp = RV_poi(gen);
+        std::gamma_distribution<float> RV_gam(a+sp, b);
+        rv[i] = RV_gam(gen);
+    }
+    return rv;
+}
+
+// rvb=rv_rg1(4., 0.2, 1., 10000);
