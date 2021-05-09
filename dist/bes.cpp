4f82ec585eb12539f82c2a850007e4a199491d55
syy <syyang@bu.edu>
2021-05-09 17:13:52 -0400

    Rewrite bes

--- dist/bes.cpp
+++ dist/bes.cpp
@@ -14,7 +14,7 @@ float log_pm_unnorm (float a, float nu, float m)
 
 
 // prob of bes(nu,a, x=m)
-float p_m(float a, float nu, float m)
+float p_bes(float a, float nu, float m)
 {
     float logpm_unnorm, pm;
     float norm_bes;
@@ -27,50 +27,56 @@ float p_m(float a, float nu, float m)
     return pm;
 }
 
+float rv_bes(float a, float nu, std::mt19937& gen)
+{
+    //return integer as float
+    float m;
+    m = std::round( (std::sqrt(a*a+nu*nu) - nu)/2. );
+
+    float pm, prob_mNx;
+    pm = p_bes(a, nu, m);
+    float w = 1. + pm/2.;
+
+    float u1, u2;
+    float x, y, j;
+    bool s;
+
+    std::uniform_real_distribution<float> RV_u(0., 1.);
+    std::exponential_distribution<float> RV_e(1.);
+    std::bernoulli_distribution RV_b(0.5);
+
+    do{
+        u1 = RV_u(gen);
+        u2 = RV_u(gen);
+        s = RV_b(gen);
+        y = (u1 < w/(1+w)) ? w*RV_u(gen)/pm : (w+RV_e(gen))/pm ;
+        x = std::round(y);
+        if (s == false) {
+            x *= -1;
+        }
+        prob_mNx = p_bes(a, nu, m+x);
+        j = std::log(w);
+        if ( (u2-pm*y)<0 ) {
+            j += u2-pm*y;
+        }
+    } while( j > std::log(prob_mNx)-std::log(pm) );
+
+    return m+x;
+}
+
+
 // maybe change to std::vector<int>
 std::vector<float> rv_bes ( float a,  float nu,  int n, unsigned seed=1111)
 {
     // nu > -1, a > 0 
     std::vector<float> rv(n);
 
-    float m;
-    m = std::round( (std::sqrt(a*a+nu*nu) - nu)/2. );
-
-    float pm;
-    pm = p_m(a, nu, m);
-
-    float w = 1. + pm/2.;
-
-    std::uniform_real_distribution<float> RV_u(0., 1.);
-    std::exponential_distribution<float> RV_e(1.);
-    std::bernoulli_distribution RV_b(0.5);
-    std::mt19937 generator (seed);
-    float u1, u2;
-    float x, y;
-    float pmx, j;
-    bool s;
-
+    std::mt19937 gen(seed);
 
     for(int i=0; i<n; i++) {
-        do{
-            u1 = RV_u(generator);
-            u2 = RV_u(generator);
-            s = RV_b(generator);
-            y = (u1 < w/(1+w)) ? w*RV_u(generator)/pm : (w+RV_e(generator))/pm ;
-            x = std::round(y);
-            if (s == false) {
-                x *= -1;
-            }
-            pmx = p_m(a, nu, m+x);
-            j = std::log(w);
-            if ( (u2-pm*y)<0 ) {
-                j += u2-pm*y;
-            }
-        } while( j > std::log(pmx)-std::log(pm) );
-        rv[i] = m+x;
+        rv[i] = rv_bes(a, nu, gen);
     }
 
     return rv;
-
 }
 
